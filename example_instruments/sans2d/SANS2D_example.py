7377880bc8c87c3acad3eb2fe21481403020236a
Matthew D Jones <matthew.d.jones@tessella.com>
2017-06-07 17:27:46 +0100

    Reorder the SANS2D nexus file

--- example_instruments/sans2d/SANS2D_example.py
+++ example_instruments/sans2d/SANS2D_example.py
@@ -29,7 +29,6 @@ if __name__ == '__main__':
     # builder = NexusBuilder('SANS_example_blosc.hdf5', 'SANS_test.nxs', idf_filename='SANS2D_Definition_Tubes.xml',
     #                        compress_type=32001)
     builder.copy_items(copy_l_to_r)
-    builder.add_user('Sans2d Team', 'ISIS, STFC')
     builder.add_dataset('sample', 'distance', 19.281)
     sample_transform_group = builder.add_transformation_group('sample')
     sample_position = builder.add_transformation(sample_transform_group, 'translation', 19.281, 'metres',
@@ -42,15 +41,36 @@ if __name__ == '__main__':
     builder.add_monitor(3, 19.497 - 19.281, 19.497)
     builder.add_monitor(4, 30.0 - 19.281, 30.0)
 
-    # Define monitor_1 to have the shape of the Utah teapot as example use of NXshape
-    builder.add_shape_from_file('teapot.off', 'instrument/monitor_1', 'shape')
-
     # Add the first detector panel
     # This panel is centred on the beam centre
     # 4.0 is the displacement along the beam (z-axis) from the sample (L2 distance)
     detector_group_1 = builder.add_detector('rear-detector', 1)
+    left_tubes = builder.add_grid_pattern(detector_group_1, 'left_tubes', 1100000, [-0.5192, -0.48195, 4.0],
+                                          [512, 60],
+                                          [1, 2000], ([0.002033984375, 0, 0], [0, 0.0162, 0]),
+                                          depends_on=sample_position)
+    builder.add_tube_pixel(left_tubes, 0.002033984375, 0.00405)
+    right_tubes = builder.add_grid_pattern(detector_group_1, 'right_tubes', 1101000, [-0.5222, -0.473855, 4.0],
+                                           [512, 60], [1, 2000], ([0.002033984375, 0, 0], [0, 0.0162, 0]),
+                                           depends_on=sample_position)
+    builder.add_tube_pixel(right_tubes, 0.002033984375, 0.00405)
 
-    # Copy measured data from the existing NeXus file
+    # Add the second detector panel
+    # This panel is displaced by -1.1m on the x axis from the beam centre
+    detector_group_2 = builder.add_detector('front-detector', 2)
+    left_tubes_2 = builder.add_grid_pattern(detector_group_2, 'left_tubes', 1100000, [-0.5192 - 1.1, -0.48195, 4.0],
+                                            [512, 60], [1, 2000], ([0.002033984375, 0, 0], [0, 0.0162, 0]),
+                                            depends_on=sample_position)
+    builder.add_tube_pixel(left_tubes_2, 0.002033984375, 0.00405)
+    right_tubes_2 = builder.add_grid_pattern(detector_group_2, 'right_tubes', 1101000, [-0.5222 - 1.1, -0.473855, 4.0],
+                                             [512, 60], [1, 2000], ([0.002033984375, 0, 0], [0, 0.0162, 0]),
+                                             depends_on=sample_position)
+    builder.add_tube_pixel(right_tubes_2, 0.002033984375, 0.00405)
+
+    # Copy data from the existing NeXus file and add some other bits to flesh out the example file
+    builder.add_user('Sans2d Team', 'ISIS, STFC')
+    # Define monitor_1 to have the shape of the Utah teapot as example use of NXshape
+    builder.add_shape_from_file('teapot.off', 'instrument/monitor_1', 'shape')
     builder.copy_items(OrderedDict([('raw_data_1/detector_1_events',
                                      'raw_data_1/detector_1_events'),
                                     ('raw_data_1/detector_1_events/event_id',
@@ -76,24 +96,3 @@ if __name__ == '__main__':
                                     ('raw_data_1/monitor_4/time_of_flight',
                                      'raw_data_1/instrument/monitor_4/time_of_flight'),
                                     ]))
-    left_tubes = builder.add_grid_pattern(detector_group_1, 'left_tubes', 1100000, [-0.5192, -0.48195, 4.0],
-                                          [512, 60],
-                                          [1, 2000], ([0.002033984375, 0, 0], [0, 0.0162, 0]),
-                                          depends_on=sample_position)
-    builder.add_tube_pixel(left_tubes, 0.002033984375, 0.00405)
-    right_tubes = builder.add_grid_pattern(detector_group_1, 'right_tubes', 1101000, [-0.5222, -0.473855, 4.0],
-                                           [512, 60], [1, 2000], ([0.002033984375, 0, 0], [0, 0.0162, 0]),
-                                           depends_on=sample_position)
-    builder.add_tube_pixel(right_tubes, 0.002033984375, 0.00405)
-
-    # Add the second detector panel
-    # This panel is displaced by -1.1m on the x axis from the beam centre
-    detector_group_2 = builder.add_detector('front-detector', 2)
-    left_tubes_2 = builder.add_grid_pattern(detector_group_2, 'left_tubes', 1100000, [-0.5192 - 1.1, -0.48195, 4.0],
-                                            [512, 60], [1, 2000], ([0.002033984375, 0, 0], [0, 0.0162, 0]),
-                                            depends_on=sample_position)
-    builder.add_tube_pixel(left_tubes_2, 0.002033984375, 0.00405)
-    right_tubes_2 = builder.add_grid_pattern(detector_group_2, 'right_tubes', 1101000, [-0.5222 - 1.1, -0.473855, 4.0],
-                                             [512, 60], [1, 2000], ([0.002033984375, 0, 0], [0, 0.0162, 0]),
-                                             depends_on=sample_position)
-    builder.add_tube_pixel(right_tubes_2, 0.002033984375, 0.00405)
