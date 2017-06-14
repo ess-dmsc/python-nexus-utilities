a302e67e89ba2951b646dfea24d44e0202b9a6f9
Matthew D Jones <matthew.d.jones@tessella.com>
2017-06-14 16:22:25 +0100

    Refactor IDFParser test

--- tests/test_idfparser.py
+++ tests/test_idfparser.py
@@ -4,11 +4,16 @@ from io import StringIO
 from idfparser import IDFParser
 
 
-def test_get_instrument_name():
-    instrument_name = 'TEST'
+def get_fake_idf_file(instrument_name='TEST'):
     fake_idf_file = StringIO('<?xml version="1.0" encoding="UTF-8"?>\n'
                              '<instrument xmlns="http://www.mantidproject.org/IDF/1.0"\n'
                              'name="' + instrument_name + '">\n'
                                                           '</instrument>')
+    return fake_idf_file
+
+
+def test_get_instrument_name():
+    instrument_name = 'TEST_NAME'
+    fake_idf_file = get_fake_idf_file(instrument_name)
     parser = IDFParser(fake_idf_file)
     assert parser.get_instrument_name() == instrument_name
