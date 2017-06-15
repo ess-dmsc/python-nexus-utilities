f1b35f83dd130fbd513ec2889b2f761d378b183b
Matthew D Jones <matthew.d.jones@tessella.com>
2017-06-15 12:13:01 +0100

    More IDFParser tests

--- tests/test_idfparser.py
+++ tests/test_idfparser.py
@@ -5,7 +5,7 @@ from io import StringIO
 from idfparser import IDFParser
 
 
-def get_fake_idf_file(instrument_name='TEST', source_name=None, sample=None, defaults=None):
+def create_fake_idf_file(instrument_name='TEST', source_name=None, sample=None, defaults=None, monitors=None):
     fake_idf_file = StringIO()
 
     # instrument
@@ -23,13 +23,23 @@ def get_fake_idf_file(instrument_name='TEST', source_name=None, sample=None, def
         fake_idf_file.write(
             '<component type="' + sample['name'] + '"><location x="' + str(sample['position'][0]) + '" y="' + str(
                 sample['position'][1]) + '" z="' + str(sample['position'][2]) + '"/></component>')
-        fake_idf_file.write('<type name="' + sample['name'] + '" is="SamplePos"/>')
+        fake_idf_file.write('<type name="' + sample['name'] + '" is="SamplePos"/>\n')
 
     # defaults
     if defaults is not None:
         fake_idf_file.write('<defaults><length unit="' + defaults['length_units'] + '"/><angle unit="' + defaults[
             'angle_units'] + '"/><reference-frame><along-beam axis="z"/>'
-                             '<pointing-up axis="y"/><handedness val="right"/></reference-frame></defaults>')
+                             '<pointing-up axis="y"/><handedness val="right"/></reference-frame></defaults>\n')
+
+    # monitors
+    if monitors is not None:
+        fake_idf_file.write('<component type="monitors" idlist="monitors"><location/></component>\n')
+        fake_idf_file.write('<type name="monitors"><component type="monitor-tbd">'
+                            '<location z="7.217" name="' + monitors['name'] + '"/></component></type>\n')
+        fake_idf_file.write('<type name="monitor-tbd" is="monitor"><cylinder id="some-shape"><centre-of-bottom-base '
+                            'r="0.0" t="0.0" p="0.0" /><axis x="0.0" y="0.0" z="1.0" /> <radius val="0.01" />'
+                            '<height val="0.03" /></cylinder></type>\n')
+        fake_idf_file.write('<idlist idname="monitors"><id start="1" end="1" /></idlist>\n')
 
     fake_idf_file.write('</instrument>\n')
     fake_idf_file.seek(0)  # So that the xml parser reads from the start of the file
@@ -38,7 +48,7 @@ def get_fake_idf_file(instrument_name='TEST', source_name=None, sample=None, def
 
 def test_get_instrument_name():
     instrument_name = 'TEST_NAME'
-    fake_idf_file = get_fake_idf_file(instrument_name)
+    fake_idf_file = create_fake_idf_file(instrument_name)
     parser = IDFParser(fake_idf_file)
     assert parser.get_instrument_name() == instrument_name
     fake_idf_file.close()
@@ -46,7 +56,7 @@ def test_get_instrument_name():
 
 def test_get_source_name():
     name = 'TEST_SOURCE'
-    fake_idf_file = get_fake_idf_file(source_name=name)
+    fake_idf_file = create_fake_idf_file(source_name=name)
     parser = IDFParser(fake_idf_file)
     assert parser.get_source_name() == name
     fake_idf_file.close()
@@ -55,7 +65,7 @@ def test_get_source_name():
 def test_get_sample_position():
     test_sample = {'name': 'TEST_SAMPLE',
                    'position': [-0.54, 42.0, 0.48]}
-    fake_idf_file = get_fake_idf_file(sample=test_sample)
+    fake_idf_file = create_fake_idf_file(sample=test_sample)
     parser = IDFParser(fake_idf_file)
     np.testing.assert_allclose(parser.get_sample_position(), test_sample['position'])
     fake_idf_file.close()
@@ -64,25 +74,34 @@ def test_get_sample_position():
 def test_get_length_units():
     test_defaults = {'length_units': 'cubits',
                      'angle_units': 'deg'}
-    fake_idf_file = get_fake_idf_file(defaults=test_defaults)
+    fake_idf_file = create_fake_idf_file(defaults=test_defaults)
     parser = IDFParser(fake_idf_file)
     assert parser.get_length_units() == test_defaults['length_units']
     fake_idf_file.close()
 
 
 def test_get_angle_units():
-    test_defaults = {'length_units': 'cubits',
+    test_defaults = {'length_units': 'Yojana',
                      'angle_units': 'deg'}
-    fake_idf_file = get_fake_idf_file(defaults=test_defaults)
+    fake_idf_file = create_fake_idf_file(defaults=test_defaults)
     parser = IDFParser(fake_idf_file)
     assert parser.get_angle_units() == test_defaults['angle_units']
     fake_idf_file.close()
 
 
 def test_angle_unit_other_than_rad_or_deg_fails():
-    test_defaults = {'length_units': 'cubits',
+    test_defaults = {'length_units': 'Ald',
                      'angle_units': 'Furman'}
-    fake_idf_file = get_fake_idf_file(defaults=test_defaults)
+    fake_idf_file = create_fake_idf_file(defaults=test_defaults)
     with pytest.raises(ValueError, message="Expecting ValueError for unexpected angle unit"):
         IDFParser(fake_idf_file)
+    fake_idf_file.close()
+
+
+def test_get_monitors():
+    monitors = {'name': 'TEST_MONITOR'}
+    fake_idf_file = create_fake_idf_file(monitors=monitors)
+    parser = IDFParser(fake_idf_file)
+    monitors_out, monitor_types = parser.get_monitors()
+    assert monitors_out[0]['name'] == monitors['name']
     fake_idf_file.close()
