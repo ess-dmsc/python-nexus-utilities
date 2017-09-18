5aa700188efc7bc6491c3fb9d3074ae5aa82a8b6
Matthew D Jones <matthew.d.jones@tessella.com>
2017-09-18 13:15:31 +0100

    Re #47 Rename fake instrument IDF for consistency

--- example_instruments/smallfake/SMALLFAKE_example.py
+++ example_instruments/smallfake/SMALLFAKE_example.py
@@ -4,7 +4,7 @@ from detectorplotter import DetectorPlotter
 if __name__ == '__main__':
     output_filename = 'SMALLFAKE_example_geometry.hdf5'
     # compress_type=32001 for BLOSC, or don't specify compress_type and opts to get non-compressed datasets
-    builder = NexusBuilder(output_filename, idf_file='minimal_geometry.xml', compress_type='gzip', compress_opts=1)
+    builder = NexusBuilder(output_filename, idf_file='SMALLFAKE_Definition.xml', compress_type='gzip', compress_opts=1)
 
     builder.add_instrument_geometry_from_idf()
     del builder
