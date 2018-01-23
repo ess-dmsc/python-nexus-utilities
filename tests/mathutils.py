9f43721def30948d3b7f59acdedc2a3a0d0b9103
Owen Arnold <owen.arnold@stfc.ac.uk>
2018-01-23 16:26:51 +0000

    fix typo

--- tests/mathutils.py
+++ tests/mathutils.py
@@ -1,7 +1,7 @@
 import cmath
 def isclose(x, y, rel_tol=1e-9, abs_tol=0.0):
     try:
-        return cmath.isclose(x, y, rel_to, abs_tol)
+        return cmath.isclose(x, y, rel_tol, abs_tol)
     except AttributeError:
         return abs(x-y) <= max(rel_tol * max(abs(x), abs(y)), abs_tol)
 
