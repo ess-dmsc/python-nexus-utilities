78a654ebe01df80ce65d5849db5007634af6e2f2
Matthew D Jones <matthew.d.jones@tessella.com>
2018-01-31 16:22:28 +0000

    add options to create and display OFF file

--- build.py
+++ build.py
@@ -3,6 +3,7 @@
 import argparse
 import os
 from nexusbuilder import NexusBuilder
+from nexustooff import nexus_geometry_to_off_file
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -14,22 +15,27 @@ if __name__ == '__main__':
     optional_args = parser.add_argument_group('Optional arguments')
     optional_args.add_argument('-p', '--plot', action='store_true', default=False,
                                help='Plots the geometry in the DetectorPlotter')
-    optional_args.add_argument('-d', '--outputdirectory', default=None,
-                               help='The output directory where the generated nexus file should go')
-    optional_args.add_argument('-f', '--outputfilename', default=None,
-                               help='The filename desired for the generated nexus file')
+    optional_args.add_argument('-d', '--output-directory', default=None,
+                               help='The output directory where the generated NeXus file should go')
+    optional_args.add_argument('-f', '--output-filename', default=None,
+                               help='The filename desired for the generated NeXus file')
+    optional_args.add_argument('-o', '--off-filename', default=None,
+                               help='Creates OFF file of full geometry with this filename')
+    optional_args.add_argument('-r', '--render', action='store_true', default=False,
+                               help='Render 3D view of output OFF file, must be used with -o')
 
     arguments = parser.parse_args()
 
     if not os.path.exists(arguments.IDF):
         raise ValueError('Specified IDF {} does not exist'.format(arguments.IDF))
 
-    output_dir = arguments.outputdirectory if arguments.outputdirectory else os.path.dirname(
+    output_dir = arguments.output_directory if arguments.output_directory else os.path.dirname(
         os.path.realpath(arguments.IDF))
-    output_filename = arguments.outputfilename if arguments.outputfilename else \
+    output_filename = arguments.output_filename if arguments.output_filename else \
     os.path.splitext(os.path.basename(arguments.IDF))[0] + '.hdf5'
+    nexus_file_fullpath = os.path.join(output_dir, output_filename)
 
-    with NexusBuilder(os.path.join(output_dir, output_filename), idf_file=arguments.IDF, compress_type='gzip',
+    with NexusBuilder(nexus_file_fullpath, idf_file=arguments.IDF, compress_type='gzip',
                       compress_opts=1) as builder:
         builder.add_instrument_geometry_from_idf()
 
@@ -38,3 +44,10 @@ if __name__ == '__main__':
 
         with DetectorPlotter(output_filename) as plotter:
             plotter.plot_pixel_positions()
+
+    if arguments.off_filename:
+        off_file_fullpath = os.path.join(output_dir, arguments.off_filename)
+        nexus_geometry_to_off_file(nexus_file_fullpath, off_file_fullpath)
+        if arguments.render:
+            from drawoff import render_off_from_file
+            render_off_from_file(off_file_fullpath)
