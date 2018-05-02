355bb88f233e338dabc6d104839148ee30cfbfe8
Matthew D Jones <matthew.d.jones@tessella.com>
2018-05-02 17:18:12 +0100

    Reduce size of output file by truncating input data

--- example_instruments/sans2d/SANS2D_example.py
+++ example_instruments/sans2d/SANS2D_example.py
@@ -4,24 +4,15 @@ from detectorplotter import DetectorPlotter
 from nxloghelper import add_example_nxlog
 
 
-def __truncate_dataset(dataset_name, truncate_to_size):
-    """
-    Truncate dataset to specified size
-    There is a resize method for datasets but it only works for chunked data, this method should always work
-    """
-    dataset = builder.get_root()['detector_1_events/'+dataset_name][:truncate_to_size]
-    dataset_attrs = builder.get_root()['detector_1_events/'+dataset_name].attrs
-    del builder.get_root()['detector_1_events/'+dataset_name]
-    builder.add_dataset(builder.get_root()['detector_1_events'], dataset_name, dataset, dataset_attrs)
-
-
-def __truncate_event_data():
-    __truncate_dataset('event_index', 10)
-    __truncate_dataset('event_time_zero', 10)
-    __truncate_dataset('event_id', 7014)
-    __truncate_dataset('event_time_offset', 7014)
-    del builder.get_root()['detector_1_events/total_counts']
-    builder.add_dataset(builder.get_root()['detector_1_events'], 'total_counts', 7014)
+def __copy_and_truncate(builder, source_dataset_path, target_dataset_path, truncate_to_size):
+    source_dataset = builder.source_file.get(source_dataset_path)[:truncate_to_size]
+    source_attrs = builder.source_file.get(source_dataset_path).attrs
+    if target_dataset_path[0] == "/":
+        target_dataset_path = target_dataset_path[0:]
+    target_parent_path = "".join(target_dataset_path.split('/')[1:-1])
+    target_dataset_name = target_dataset_path.split('/')[-1]
+    builder.add_dataset(builder.get_root()[target_parent_path], target_dataset_name, source_dataset,
+                        attributes=source_attrs)
 
 
 def __copy_existing_data():
@@ -34,8 +25,7 @@ def __copy_existing_data():
                                      nx_entry_name + '/instrument/moderator/distance'),
                                     ('raw_data_1/instrument/source/probe',
                                      nx_entry_name + '/instrument/source/probe'),
-                                    (
-                                        'raw_data_1/instrument/source/type', nx_entry_name + '/instrument/source/type'),
+                                    ('raw_data_1/instrument/source/type', nx_entry_name + '/instrument/source/type'),
                                     ('raw_data_1/sample/name', nx_entry_name + '/sample/name'),
                                     ('raw_data_1/sample/type', nx_entry_name + '/sample/type'),
                                     ('raw_data_1/duration', nx_entry_name + '/duration'),
@@ -43,18 +33,6 @@ def __copy_existing_data():
                                     ('raw_data_1/end_time', nx_entry_name + '/end_time'),
                                     ('raw_data_1/run_cycle', nx_entry_name + '/run_cycle'),
                                     ('raw_data_1/title', nx_entry_name + '/title'),
-                                    ('raw_data_1/detector_1_events',
-                                     nx_entry_name + '/detector_1_events'),
-                                    ('raw_data_1/detector_1_events/event_id',
-                                     nx_entry_name + '/detector_1_events/event_id'),
-                                    ('raw_data_1/detector_1_events/total_counts',
-                                     nx_entry_name + '/detector_1_events/total_counts'),
-                                    ('raw_data_1/detector_1_events/event_index',
-                                     nx_entry_name + '/detector_1_events/event_index'),
-                                    ('raw_data_1/detector_1_events/event_time_zero',
-                                     nx_entry_name + '/detector_1_events/event_time_zero'),
-                                    ('raw_data_1/detector_1_events/event_time_offset',
-                                     nx_entry_name + '/detector_1_events/event_time_offset'),
                                     ('raw_data_1/monitor_1/data', nx_entry_name + '/instrument/monitor1/data'),
                                     ('raw_data_1/monitor_1/time_of_flight',
                                      nx_entry_name + '/instrument/monitor1/time_of_flight'),
@@ -82,7 +60,17 @@ if __name__ == '__main__':
         builder.add_shape_from_file('../off_files/teapot.off', 'instrument/monitor1', 'shape')
 
         __copy_existing_data()
-        __truncate_event_data()
+
+        builder.add_nx_group(builder.get_root(), 'detector_1_events', 'NXevent_data')
+
+        __copy_and_truncate(builder, 'raw_data_1/detector_1_events/event_id', 'raw_data_1/detector_1_events/event_id',
+                            7014)
+        __copy_and_truncate(builder, 'raw_data_1/detector_1_events/event_index',
+                            'raw_data_1/detector_1_events/event_index', 10)
+        __copy_and_truncate(builder, 'raw_data_1/detector_1_events/event_time_zero',
+                            'raw_data_1/detector_1_events/event_time_zero', 7014)
+        __copy_and_truncate(builder, 'raw_data_1/detector_1_events/event_time_offset',
+                            'raw_data_1/detector_1_events/event_time_offset', 10)
 
         add_example_nxlog(builder, '/' + nx_entry_name + '/sample/', 10)
 
