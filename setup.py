4c355cecee3e191788b83531c1ffcc518d0ebc6e
Matthew D Jones <matthew.d.jones@tessella.com>
2020-12-14 12:15:50 +0000

    Remove tables req

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@
 
 from distutils.core import setup
 
-setup(name='nexusutils',
-      version='1.0',
-      description='Python NeXus Utilities',
-      url='https://github.com/ess-dmsc/python-nexus-utilities',
-      packages=['nexusutils'],
-      install_requires=['appdirs',
-                        'h5py',
-                        'numexpr',
-                        'numpy',
-                        'packaging',
-                        'pyparsing',
-                        'tables',
-                        'matplotlib',
-                        'tabulate']
-      )
+setup(
+    name="nexusutils",
+    version="1.0",
+    description="Python NeXus Utilities",
+    url="https://github.com/ess-dmsc/python-nexus-utilities",
+    packages=["nexusutils"],
+    install_requires=[
+        "appdirs",
+        "h5py",
+        "numexpr",
+        "numpy",
+        "packaging",
+        "pyparsing",
+        "matplotlib",
+        "tabulate",
+    ],
+)
